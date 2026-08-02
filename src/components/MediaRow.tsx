d7db527265300b9f26a5e66be3fe0d05441dd2f6
lol123-creator <indiana1269@gmail.com>
2026-08-02 02:37:36 +0200

    Wrap ScrollRow in RevealOnScroll component

--- src/components/MediaRow.tsx
+++ src/components/MediaRow.tsx
@@ -1,5 +1,6 @@
 import { MediaCard } from '@/components/MediaCard';
 import { ScrollRow } from '@/components/ScrollRow';
+import { RevealOnScroll } from '@/components/RevealOnScroll';
 import type { MediaItem } from '@/types/media';
 
 interface MediaRowProps {
@@ -11,14 +12,16 @@ export function MediaRow({ title, items }: MediaRowProps) {
   if (items.length === 0) return null;
 
   return (
-    <ScrollRow title={title}>
-      {items.map((item, i) => (
-        <MediaCard
-          key={`${title}-${item.type}-${item.id}-${i}`}
-          item={item}
-          priority={i < 4}
-        />
-      ))}
-    </ScrollRow>
+    <RevealOnScroll>
+      <ScrollRow title={title}>
+        {items.map((item, i) => (
+          <MediaCard
+            key={`${title}-${item.type}-${item.id}-${i}`}
+            item={item}
+            priority={i < 4}
+          />
+        ))}
+      </ScrollRow>
+    </RevealOnScroll>
   );
 }
